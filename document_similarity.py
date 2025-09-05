fe046f20b7f3aba3f74c03045b7836571c170a56
Kent@44240 <srinunayakk7@gmail.com>
2025-09-05 08:21:42 -0400

    completed the project

--- document_similarity.py
+++ document_similarity.py
@@ -1,2 +1,31 @@
-import langchain
-print(langchain.__version__)
+from langchain.embeddings import HuggingFaceEmbeddings
+from dotenv import load_dotenv
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+load_dotenv()
+
+
+documents = [
+    "Delhi is the capital of India",
+    "Kolkata is the capital of West Bengal",
+    "Paris is the capital of France"
+]
+
+embeddings = HuggingFaceEmbeddings(
+    model_name = "sentence-transformers/all-MiniLM-L6-v2"
+)
+
+query = "Which city is the capital of France?"
+
+ 
+
+result = embeddings.embed_documents(documents)
+query_embedding = embeddings.embed_query(query)
+
+scores = cosine_similarity([query_embedding], result)[0]
+
+index, score = sorted(list(enumerate(scores)), key=lambda x:x[1])[-1]
+
+print(documents[index])
+print("similarity score", score)
